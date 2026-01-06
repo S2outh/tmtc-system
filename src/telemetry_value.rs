3a6344618187464f796976198a8d7d5d3c616df0
eleocraft <eliseo.cailloux@gmx.de>
2026-01-06 14:33:09 +0100

    Option<T>
    + implementing TMValue and DynTMValue for Option<T>

--- src/telemetry_value.rs
+++ src/telemetry_value.rs
@@ -116,3 +116,38 @@ impl<const N: usize, T: TMValue> DynTMValue for Vec<T, N> {
 impl<const N: usize, T: TMValue> TMValue for Vec<T, N> {
     const BYTE_SIZE: usize = N * T::BYTE_SIZE;
 }
+
+// # Options
+impl<T: TMValue> DynTMValue for Option<T> {
+    fn read(&mut self, bytes: &[u8]) -> Result<usize, TMValueError> {
+        let mut pos = 1;
+        match bytes[0] {
+            0u8 => {
+                *self = None;
+            }
+            1u8 => unsafe {
+                let mut v0: T = core::mem::zeroed();
+                pos += v0.read(&bytes[pos..])?;
+                *self = Some(v0);
+            },
+            _ => return Err(TMValueError::BadEnumVariant),
+        }
+        Ok(pos)
+    }
+    fn write(&self, mem: &mut [u8]) -> Result<usize, TMValueError> {
+        let mut pos = 1;
+        match self {
+            None => {
+                mem[0] = 0u8;
+            }
+            Some(v0) => {
+                mem[0] = 1u8;
+                pos += v0.write(&mut mem[pos..])?;
+            }
+        }
+        Ok(pos)
+    }
+}
+impl<T: TMValue> TMValue for Option<T> {
+    const BYTE_SIZE: usize = 1 + T::BYTE_SIZE;
+}
